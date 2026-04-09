450fcdb12afdcfe301158ecdc4700adf72dc52dc
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 16:34:20 +0200

    execute() gère un tableau de produits - test encore rouge

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,8 +1,8 @@
 import { describe, test, expect, beforeEach } from "vitest";
 
 class CalculatePriceUseCase {
-    execute(product: { price: number; name: string }) {
-        return product.price;
+    execute(products:{ price: number; name: string }[]) {
+        return products[0].price;
     }
 }
 
@@ -18,18 +18,19 @@ describe ("CalculatePriceUseCase", ()=>{
     //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
     test("For CalculatePriceUseCase", () => {
 
-        expect(calculatePrice.execute({price: 1, name: "product1"})).toBe(1);
+        expect(calculatePrice.execute([{price: 1, name: "product1"}])).toBe(1);
     });
 
     //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
     //5. test pass with syntaxe error Expected 0 arguments, but got 1
     //6 test pass return product price
     test("For one product", () => {
-        expect(calculatePrice.execute({price: 100, name: "product1"})
+        expect(calculatePrice.execute([{price: 100, name: "product1"}])
         ).toBe(100);
     });
 
     //7.test failed CalculatePriceUseCase > For two products :expected undefined to be 200
+    //8. modification de la signature de execute() pour accepter un tableau de produits
     test("For two products", () => {
         expect(calculatePrice.execute([{price: 100, name: "product1"},{price: 100, name: "product2"}])
         ).toBe(200);
