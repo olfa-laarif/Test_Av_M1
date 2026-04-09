3fde2fefbff8b1e30b3388521362fab1d16a3939
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 17:25:58 +0200

    mise à jour des commentaires

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -2,7 +2,7 @@ import { describe, test, expect, beforeEach } from "vitest";
 
 class CalculatePriceUseCase {
     execute(products:{ price: number; name: string ,quantity: number }[]) {
-        //return total price of products
+        //Retourne le prix total des produits
         return products.reduce((sum, product) => sum + product.price * product.quantity , 0);
     }
 }
@@ -10,36 +10,36 @@ class CalculatePriceUseCase {
 describe ("CalculatePriceUseCase", ()=>{
     //4.deplacer le calculatePrice
     let calculatePrice= new CalculatePriceUseCase();
-    //1 er test
+    // Premier test
     test("firstTest",()=>{
         expect(true).toBeTruthy;
     });
 
-    //1.test failed ReferenceError: CalculatePriceUseCase is not defined
-    //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
+    //1. Test échoue : ReferenceError CalculatePriceUseCase non définie
+    //2.Test passe
     test("For CalculatePriceUseCase", () => {
 
         expect(calculatePrice.execute([{price: 1, name: "product1",quantity: 1}])).toBe(1);
     });
 
-    //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
-    //5. test pass with syntaxe error Expected 0 arguments, but got 1
-    //6 test pass return product price
+    //3.Test échoue : calculatePrice non défini
+    //5. Test passe avec erreur de la syntaxe (Expected 0 arguments, but got 1
+    //6 test passe: Implémentation minimale : retourner le prix du produit
     test("For one product", () => {
         expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1}])
         ).toBe(100);
     });
 
-    //7.test failed CalculatePriceUseCase > For two products :expected undefined to be 200
-    //8. modification de la signature de execute() pour accepter un tableau de produits
-    //9. test passes
+    //7.Test échoue : gestion de plusieurs produits manquante
+    //8.  Modification de la méthode execute pour accepter un tableau
+    //9. test passe
     test("For two products", () => {
         expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1},{price: 100, name: "product2",quantity: 1}])
         ).toBe(200);
     });
 
-    //10.test failed with error syntaxe quantity does not exist in type { price: number; name: string;}
-    //11.test passes - add quantity to product
+    //10.Test échoue : la propriété quantity n'existe pas
+    //11.Test passe : ajout de quantity dans le Product et Mise à jour du calcul pour prendre en compte la quantité
     test("For two products with quantity", () => {
         expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1,},{price: 100, name: "product2",quantity: 2}])
         ).toBe(300);
