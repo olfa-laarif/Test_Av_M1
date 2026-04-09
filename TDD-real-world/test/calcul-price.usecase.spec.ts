3c7c05f1f7ea8795110aaa25617e79388d5f5911
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 15:27:18 +0200

    le test pour un produit ne compile pas - calculatePrice non défini

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,25 +1,29 @@
 import { describe, test, expect, beforeEach } from "vitest";
 
 class CalculatePriceUseCase {
-    execute(param: any[]) {
+    execute() {
         return 0;
     }
 }
 
 describe ("CalculatePriceUseCase", ()=>{
 
-
     //1 er test
     test("firstTest",()=>{
         expect(true).toBeTruthy;
     });
 
-
-    //test failed ReferenceError: CalculatePriceUseCase is not defined
-    //test pass CalculatePriceUseCase > For CalculatePriceUseCase
+    //1.test failed ReferenceError: CalculatePriceUseCase is not defined
+    //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
     test("For CalculatePriceUseCase", () => {
         let calculatePrice= new CalculatePriceUseCase();
-        expect(calculatePrice.execute([])).toBe(0);
+        expect(calculatePrice.execute()).toBe(0);
+    });
+
+    //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
+    test("For one product", () => {
+        expect(calculatePrice.execute({price: 1, name: "product1",},),
+        ).toBe(1);
     });
 
 
