3a70fafa748b11bb7f933797919937e3f570ad9f
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 17:13:34 +0200

    test passe: ajout de quantity dans le Product et prise en compte dans le calcul

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,9 +1,9 @@
 import { describe, test, expect, beforeEach } from "vitest";
 
 class CalculatePriceUseCase {
-    execute(products:{ price: number; name: string }[]) {
+    execute(products:{ price: number; name: string ,quantity: number }[]) {
         //return total price of products
-        return products.reduce((sum, product) => sum + product.price, 0);
+        return products.reduce((sum, product) => sum + product.price * product.quantity , 0);
     }
 }
 
@@ -19,26 +19,27 @@ describe ("CalculatePriceUseCase", ()=>{
     //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
     test("For CalculatePriceUseCase", () => {
 
-        expect(calculatePrice.execute([{price: 1, name: "product1"}])).toBe(1);
+        expect(calculatePrice.execute([{price: 1, name: "product1",quantity: 1}])).toBe(1);
     });
 
     //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
     //5. test pass with syntaxe error Expected 0 arguments, but got 1
     //6 test pass return product price
     test("For one product", () => {
-        expect(calculatePrice.execute([{price: 100, name: "product1"}])
+        expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1}])
         ).toBe(100);
     });
 
     //7.test failed CalculatePriceUseCase > For two products :expected undefined to be 200
     //8. modification de la signature de execute() pour accepter un tableau de produits
-    //9. test pass
+    //9. test passes
     test("For two products", () => {
-        expect(calculatePrice.execute([{price: 100, name: "product1"},{price: 100, name: "product2"}])
+        expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1},{price: 100, name: "product2",quantity: 1}])
         ).toBe(200);
     });
 
     //10.test failed with error syntaxe quantity does not exist in type { price: number; name: string;}
+    //11.test passes - add quantity to product
     test("For two products with quantity", () => {
         expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1,},{price: 100, name: "product2",quantity: 2}])
         ).toBe(300);
