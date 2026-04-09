d0cdcf5da66a341156545e9c17e3fa3527ce8302
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 16:25:15 +0200

    test ne compile pas : calcul prix pour deux produits - execute() ne gère pas les tableaux

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -18,16 +18,21 @@ describe ("CalculatePriceUseCase", ()=>{
     //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
     test("For CalculatePriceUseCase", () => {
 
-        expect(calculatePrice.execute({price: 1, name: "product1"})).toBe(0);
+        expect(calculatePrice.execute({price: 1, name: "product1"})).toBe(1);
     });
 
     //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
     //5. test pass with syntaxe error Expected 0 arguments, but got 1
     //6 test pass return product price
     test("For one product", () => {
-        expect(calculatePrice.execute({price: 100, name: "product1"}),
+        expect(calculatePrice.execute({price: 100, name: "product1"})
         ).toBe(100);
     });
 
+    //7.test failed CalculatePriceUseCase > For two products :expected undefined to be 200
+    test("For two products", () => {
+        expect(calculatePrice.execute([{price: 100, name: "product1"},{price: 100, name: "product2"}])
+        ).toBe(200);
+    });
 
 });
