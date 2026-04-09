ecea4c6eb7d174124c6cdc0f8dbf7aaaede8c2dd
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 15:09:18 +0200

    Ce test ne compile même pas — CalculatePriceUseCase n'existe pas encore

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,12 +1,19 @@
-import { describe, test, expect } from "vitest";
+import { describe, test, expect, beforeEach } from "vitest";
 
 describe ("CalculatePriceUseCase", ()=>{
+
+
     //1 er test
     test("firstTest",()=>{
         expect(true).toBeTruthy;
     });
 
 
+    //test failed ReferenceError: CalculatePriceUseCase is not defined
+    test("For CalculatePriceUseCase", () => {
+        let calculatePrice= new CalculatePriceUseCase();
+        expect(calculatePrice.execute([])).toBe(0);
+    });
 
 
 });
