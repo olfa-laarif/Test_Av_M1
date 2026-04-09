d94e5f765e43b3440025a778afdc4c7125081c16
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 15:37:34 +0200

    le test passe - retourne le prix du produit

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,13 +1,14 @@
 import { describe, test, expect, beforeEach } from "vitest";
 
 class CalculatePriceUseCase {
-    execute() {
-        return 0;
+    execute(product: { price: number; name: string }) {
+        return product.price;
     }
 }
 
 describe ("CalculatePriceUseCase", ()=>{
-
+    //4.deplacer le calculatePrice
+    let calculatePrice= new CalculatePriceUseCase();
     //1 er test
     test("firstTest",()=>{
         expect(true).toBeTruthy;
@@ -16,14 +17,16 @@ describe ("CalculatePriceUseCase", ()=>{
     //1.test failed ReferenceError: CalculatePriceUseCase is not defined
     //2.test pass CalculatePriceUseCase > For CalculatePriceUseCase
     test("For CalculatePriceUseCase", () => {
-        let calculatePrice= new CalculatePriceUseCase();
-        expect(calculatePrice.execute()).toBe(0);
+
+        expect(calculatePrice.execute({price: 1, name: "product1"})).toBe(0);
     });
 
     //3. test failed CalculatePriceUseCase > For one product → calculatePrice is not defined
+    //5. test pass with syntaxe error Expected 0 arguments, but got 1
+    //6 test pass return product price
     test("For one product", () => {
-        expect(calculatePrice.execute({price: 1, name: "product1",},),
-        ).toBe(1);
+        expect(calculatePrice.execute({price: 100, name: "product1"}),
+        ).toBe(100);
     });
 
 
