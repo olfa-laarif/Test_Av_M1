9391f00a013c92fc37794efd5e32ceb9fd427a4d
olfalaarif <olfa.laarif@gmail.com>
2026-04-09 17:34:14 +0200

    refactor: extraction du type Product dans un type séparé

--- TDD-real-world/test/calcul-price.usecase.spec.ts
+++ TDD-real-world/test/calcul-price.usecase.spec.ts
@@ -1,7 +1,13 @@
 import { describe, test, expect, beforeEach } from "vitest";
 
+export type Product = {
+    name: string;
+    quantity: number;
+    price: number;
+};
+
 class CalculatePriceUseCase {
-    execute(products:{ price: number; name: string ,quantity: number }[]) {
+    execute(products:Product[]) {
         //Retourne le prix total des produits
         return products.reduce((sum, product) => sum + product.price * product.quantity , 0);
     }
@@ -12,21 +18,22 @@ describe ("CalculatePriceUseCase", ()=>{
     let calculatePrice= new CalculatePriceUseCase();
     // Premier test
     test("firstTest",()=>{
-        expect(true).toBeTruthy;
+        expect(true).toBeTruthy();
     });
 
     //1. Test échoue : ReferenceError CalculatePriceUseCase non définie
     //2.Test passe
     test("For CalculatePriceUseCase", () => {
-
-        expect(calculatePrice.execute([{price: 1, name: "product1",quantity: 1}])).toBe(1);
+        const product: Product = { price: 1, name: "product1", quantity: 1 }
+        expect(calculatePrice.execute([product])).toBe(1);
     });
 
     //3.Test échoue : calculatePrice non défini
     //5. Test passe avec erreur de la syntaxe (Expected 0 arguments, but got 1
     //6 test passe: Implémentation minimale : retourner le prix du produit
     test("For one product", () => {
-        expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1}])
+        const product: Product = { price: 100, name: "product1", quantity: 1 }
+        expect(calculatePrice.execute([product])
         ).toBe(100);
     });
 
@@ -34,14 +41,22 @@ describe ("CalculatePriceUseCase", ()=>{
     //8.  Modification de la méthode execute pour accepter un tableau
     //9. test passe
     test("For two products", () => {
-        expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1},{price: 100, name: "product2",quantity: 1}])
+        const products: Product[] = [
+            { price: 100, name: "product1", quantity: 1 },
+            { price: 100, name: "product2", quantity: 1 },
+        ];
+        expect(calculatePrice.execute(products)
         ).toBe(200);
     });
 
     //10.Test échoue : la propriété quantity n'existe pas
     //11.Test passe : ajout de quantity dans le Product et Mise à jour du calcul pour prendre en compte la quantité
     test("For two products with quantity", () => {
-        expect(calculatePrice.execute([{price: 100, name: "product1",quantity: 1,},{price: 100, name: "product2",quantity: 2}])
+        const products: Product[] = [
+            { price: 100, name: "product1", quantity: 1 },
+            { price: 100, name: "product2", quantity: 2 },
+        ];
+        expect(calculatePrice.execute(products)
         ).toBe(300);
     });
 
